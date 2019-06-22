6649ea864266dba9c445f49fcb21ed08630c9561
kang <851434669@qq.com>
2019-06-22 13:35:00 +0800

    实时

---
+++ web/src/main/java/bupt/edu/cn/web/util/realtime/CockpitListener.java
@@ -0,0 +1,52 @@
+package bupt.edu.cn.web.util.realtime;
+
+
+import bupt.edu.cn.web.pojo.DataSource;
+import bupt.edu.cn.web.pojo.Diagram;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program analysis
+ * @description: ${TODO}
+ * @author: kang
+ * @create: 2019/06/22 13:14
+ */
+public class CockpitListener {
+
+    public int cockpitId;
+
+    public Client client;
+
+    public List<Diagram> diagrams;
+
+    public List<DataSource> dataSources;
+
+    public List<FileAlterationMonitor> FileAlterationMonitors;
+
+    public CockpitListener(){
+        this.cockpitId = -1;
+        this.diagrams = new ArrayList<>();
+        this.dataSources = new ArrayList<>();
+        this.FileAlterationMonitors = new ArrayList<>();
+    }
+
+    public CockpitListener(Client client){
+        this.cockpitId = client.getCockpitId();
+        this.client = client;
+        this.diagrams = new ArrayList<>();
+        this.dataSources = new ArrayList<>();
+        this.FileAlterationMonitors = new ArrayList<>();
+    }
+
+    public CockpitListener(int cockpitId,Client client){
+        this.cockpitId = cockpitId;
+        this.client = client;
+        this.diagrams = new ArrayList<>();
+        this.dataSources = new ArrayList<>();
+        this.FileAlterationMonitors = new ArrayList<>();
+    }
+
+}
